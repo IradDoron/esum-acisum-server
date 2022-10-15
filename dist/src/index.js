e1270fc14dc05267c46876681516ff4fb0202ed2
Irad Doron <irad.doron@gmail.com>
2022-10-15 21:31:41 +0300

    add d.ts file

--- dist/src/index.js
+++ dist/src/index.js
@@ -3,12 +3,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv/config");
 const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
 const modules_1 = __importDefault(require("../data/modules"));
 const app = (0, express_1.default)();
-const PORT = process.env.PORT || 5050;
-const HOST = process.env.HOST || 'localhost';
+const PORT = process.env['PORT'];
+console.log(process.env['PORT']);
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 app.use(express_1.default.static('assets'));
@@ -31,6 +32,6 @@ app.get('/test-image', (req, res) => {
     const testImageUrl = 'module_2/piano-fingering.png';
     res.status(200).send(testImageUrl);
 });
-app.listen(PORT, HOST, () => {
-    console.log(`Server running at http://${HOST}:${PORT}/`);
+app.listen(PORT, () => {
+    console.log(`Server running at ${PORT}/`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+
 import cors from 'cors';
 import express from 'express';
 
@@ -9,14 +11,12 @@ import modules from '../data/modules';
 
 const app = express();
 
-const PORT = process.env['PORT'] || 5050 as any;
-const HOST = process.env['HOST'] || 'localhost';
+const PORT = process.env['PORT'] ;
 
-// console.log(process.env);
-
+ console.log(process.env['PORT']);
 
 app.use(express.json());      
-
+ 
 app.use(cors());   
 
 app.use(express.static('assets'));
@@ -39,12 +39,12 @@ app.get('/modules-assets', (req, res) => {
 app.get('/api', (req, res) => {
 	res.status(200).send('Hello World!');
 });
-
+  
 app.get('/test-image', (req, res) => {
 	const testImageUrl = 'module_2/piano-fingering.png';
 	res.status(200).send(testImageUrl);
 });
 
-app.listen(PORT, HOST, () => { 
-	console.log(`Server running at http://${HOST}:${PORT}/`);
-});
+app.listen(PORT, () => { 
+	console.log(`Server running at ${PORT}/`);
+});  
